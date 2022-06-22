0fef09cc52a01148d7dad6b322501d7388170022
choisohyun <sohyun4283@naver.com>
2022-06-22 20:49:36 +0900

    feat: add type check sample code

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useCounter } from '@/hooks/useCounter';
 
 const Main = () => {
   const { count, increment } = useCounter();
 
+  const env: 'development' | 'production' = useMemo(() => {
+    return import.meta.env.VITE_TEST || process.env.NODE_ENV === 'test' ? 'development' : 'production';
+  }, []);
+
   return (
     <>
       <h2 className="test">Hello Vite + React!</h2>
-      <p className="env">
-        {import.meta.env.VITE_TEST || process.env.NODE_ENV === 'test' ? 'development' : 'production'}
-      </p>
+      <p className="env">{env}</p>
       <button type="button" onClick={increment}>
         count is: {count}
       </button>
